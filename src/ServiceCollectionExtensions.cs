31e4e4aed7da5b46c7c758e4084e4c5a5669a684
Ramon D <ramo.dev@icloud.com>
2024-04-06 20:28:05 -0600

    Refactor

--- src/ServiceCollectionExtensions.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.Extensions.DependencyInjection;
-
-namespace Wasm.LocalStorage;
-
-public static class ServiceCollectionExtensions
-{
-    public static IServiceCollection AddWasmLocalStorage(this IServiceCollection services)
-        => services.AddScoped<ILocalStorageManager, LocalStorageManager>();
-}
